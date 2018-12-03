8cd1e735da33fc903a2524240af7e166e5845bd6
david96doria <david96doria@gmail.com>
2018-12-03 13:07:24 -0600

    UPDATE ADMIN CONTROLLER

--- ltv-application-1.1/src/Application/AdministracionController.java
+++ ltv-application-1.1/src/Application/AdministracionController.java
@@ -1,5 +1,23 @@
 package Application;
 
+import Database.DataBase;
+import Database.SolicitudesTable;
+import javafx.*;
+import javafx.beans.Observable;
+import javafx.collections.ObservableList;
+import javafx.scene.control.ListView;
+
 public class AdministracionController {
 
-}
+	ListView<String> Solicitudes = new ListView<String>();
+	ObservableList<String> SolicitudesItems;
+	DataBase database;
+	SolicitudesTable Solicitud;
+	
+	public void MostrarSolicitudesEntrantes() {
+		
+		
+		
+	}
+	
+}//end class
